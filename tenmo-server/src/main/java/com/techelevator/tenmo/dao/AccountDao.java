d79470bc8b60aa580e622c0d481b9b2f337017bd
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:34:26 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,17 +1,15 @@
 package com.techelevator.tenmo.dao;
 
+
 import com.techelevator.tenmo.model.Account;
 
 import java.math.BigDecimal;
 
-//*kanae/jaron
+
 public interface AccountDao {
     BigDecimal getBalance(String user);
     int findIdByAccountId(int accountId);
-
     Account getAccountByUserId(int userId);
     Account getAccountByAccountId(int accountId);
-
     void updateAccount(Account account);
-
 }

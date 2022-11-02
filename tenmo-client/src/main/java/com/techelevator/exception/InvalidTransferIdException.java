245552707f458039ed7a23657a8c158f08934949
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:07:02 -0700

    Cleaned up codes

--- tenmo-client/src/main/java/com/techelevator/exception/InvalidTransferIdException.java
+++ tenmo-client/src/main/java/com/techelevator/exception/InvalidTransferIdException.java
@@ -1,5 +1,6 @@
 package com.techelevator.exception;
 
+
 public class InvalidTransferIdException extends Exception{
     public InvalidTransferIdException(){
         super("Invalid Transfer ID. Please choose different ID.");

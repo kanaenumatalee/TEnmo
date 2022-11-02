28341572769b7812039cf64a4bdc6f2cb8a7b714
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:05:25 -0700

    Cleaned up codes

--- tenmo-client/src/main/java/com/techelevator/util/BasicLogger.java
+++ tenmo-client/src/main/java/com/techelevator/util/BasicLogger.java
@@ -1,16 +1,17 @@
 package com.techelevator.util;
 
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class BasicLogger {
 
+public class BasicLogger {
 	private static PrintWriter pw = null;
-	
 	public static void log(String message) {
 		try {
 			if (pw == null) {
@@ -24,5 +25,4 @@ public class BasicLogger {
 			throw new BasicLoggerException(e.getMessage());
 		}
 	}
-
 }

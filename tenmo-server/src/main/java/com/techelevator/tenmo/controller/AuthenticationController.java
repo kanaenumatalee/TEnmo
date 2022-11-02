3ebc1ceaaadfd0af9fba8ffdfe7429229fc38b54
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:32:00 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AuthenticationController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.techelevator.tenmo.controller;
 
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -8,24 +9,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.LoginDTO;
 import com.techelevator.tenmo.model.RegisterUserDTO;
 import com.techelevator.tenmo.model.User;
 import com.techelevator.tenmo.security.jwt.TokenProvider;
-import org.springframework.web.server.ResponseStatusException;
 
-/**
- * Controller to authenticate users.
- */
+
 @RestController
 public class AuthenticationController {
-
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private UserDao userDao;
-
+	
     public AuthenticationController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, UserDao userDao) {
         this.tokenProvider = tokenProvider;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
@@ -34,16 +32,12 @@ public class AuthenticationController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public LoginResponse login(@Valid @RequestBody LoginDTO loginDto) {
-
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = tokenProvider.createToken(authentication, false);
-        
         User user = userDao.findByUsername(loginDto.getUsername());
-
         return new LoginResponse(jwt, user);
     }
 
@@ -59,7 +53,6 @@ public class AuthenticationController {
      * Object to return as body in JWT Authentication.
      */
     static class LoginResponse {
-
         private String token;
         private User user;
 
@@ -76,13 +69,13 @@ public class AuthenticationController {
             this.token = token;
         }
 
-		public User getUser() {
-			return user;
-		}
+	public User getUser() {
+		return user;
+	}
 
-		public void setUser(User user) {
-			this.user = user;
-		}
+	public void setUser(User user) {
+		this.user = user;
+	}
     }
 }
 

bcc01814c6c03ea619f23b7ad19760b6c7f0cc7b
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:30:04 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,27 +1,30 @@
 package com.techelevator.tenmo.controller;
 
+
 import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+
 import java.security.Principal;
+
 import java.util.List;
+
 
 @RestController
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/account/")
 public class AccountController {
-
     @Autowired
     UserDao userDao;
     @Autowired
     AccountDao accountDao;
-
 
     @RequestMapping(value = "balance", method = RequestMethod.GET)
     public BigDecimal getBalance(Principal principal){
@@ -38,40 +41,18 @@ public class AccountController {
         return userDao.getUserByUserId(id);
     }
 
-    /* findIdByAccountId
-    @RequestMapping(value = "users/{accountId}", method = RequestMethod.GET)
-    public int findIdByAccountId(@PathVariable int accountId){
-        return accountDao.findIdByAccountId(accountId);
-    }*/
-
-    // getAccountByUserId
     @RequestMapping(value = "{userId}", method = RequestMethod.GET)
     public Account getAccountByUserId(@PathVariable int userId){
         return accountDao.getAccountByUserId(userId);
     }
 
-
-
-    // getAccountByAccountId
     @RequestMapping(value = "users/{accountId}",method = RequestMethod.GET)
     public Account getAccountByAccountId(@PathVariable int accountId){
         return accountDao.getAccountByAccountId(accountId);
     }
 
-    // updateAccount
     @RequestMapping(value ="users/{id}",method = RequestMethod.PUT)
     public void updateAccount(@RequestBody Account account, @PathVariable int accountId){
         accountDao.updateAccount(account);
     }
-
-
-
-    /*
-    //findIdByUsername
-    @RequestMapping(value = "users/{username}", method = RequestMethod.GET)
-    public int findIdByUsername(@PathVariable String username){
-        return userDao.findIdByUsername(username);
-    }
-    */
-
 }

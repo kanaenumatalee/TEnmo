f6bf99dd5d77e95a83664ea64089b46730ed8e34
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:33:43 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,24 +1,27 @@
 package com.techelevator.tenmo.controller;
 
+
 import com.techelevator.tenmo.dao.*;
 import com.techelevator.tenmo.exception.InsufficientBalanceException;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.TransferStatus;
 import com.techelevator.tenmo.model.TransferType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+
 import java.util.List;
+
 
 @RestController
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/transfer/")
 public class TransferController {
-
     @Autowired
     UserDao userDao;
     @Autowired
@@ -34,19 +37,15 @@ public class TransferController {
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "users/{id}", method = RequestMethod.POST)
     public void makeTransfer(@RequestBody Transfer transfer, @PathVariable long id) throws InsufficientBalanceException {
-
         BigDecimal transferAmount = transfer.getAmount();
         Account accountFrom = accountDao.getAccountByAccountId(transfer.getAccountFrom());
         Account accountTo = accountDao.getAccountByAccountId(transfer.getAccountTo());
-
         if(transfer.getTransferStatusId() == transferStatusDao.getTransferStatusByDesc("Approved").getTransferStatusId()) {
             accountFrom.sendMoney(transferAmount);
             accountTo.getMoney(transferAmount);
-
             accountDao.updateAccount(accountFrom);
             accountDao.updateAccount(accountTo);
         }
-
         transferDao.makeTransfer(transfer);
     }
 
@@ -75,16 +74,11 @@ public class TransferController {
         return transferDao.updateTransfer(transfer);
     }
 
-
-    // getPendingTransfers
     @RequestMapping(path = "users/{userId}/pending", method = RequestMethod.GET)
     public List<Transfer> getPendingTransfers(@PathVariable int userId){
         return transferDao.getPendingTransfers(userId);
     }
 
-
-
-    //TransferStatusDao
     @RequestMapping(path="transfer_status/desc/{description}", method = RequestMethod.GET)
     public TransferStatus getTransferStatusByDescription(@PathVariable String description) {
         return transferStatusDao.getTransferStatusByDesc(description);
@@ -95,14 +89,10 @@ public class TransferController {
         return transferStatusDao.getTransferStatusById(id);
     }
 
-
-
-    //transferTypeDao
     @RequestMapping(path="transfer_type/desc/{description}", method = RequestMethod.GET)
     public TransferType getTransferTypeByDescription(@PathVariable String description) {
         return transferTypeDao.getTransferTypeByDescription(description);
     }
-
 
     @RequestMapping(path="transfer_type/{transferTypeId}", method = RequestMethod.GET)
     public TransferType getTransferTypeById(@PathVariable int transferTypeId)  {
@@ -111,24 +101,18 @@ public class TransferController {
 
     @RequestMapping(path="transfers/{id}", method = RequestMethod.PUT)
     public void updateTransferStatus(@RequestBody Transfer transfer, @PathVariable int id) throws InsufficientBalanceException {
-
         // only go through with the transfer if approved
         if(transfer.getTransferStatusId() == transferStatusDao.getTransferStatusByDesc("Approved").getTransferStatusId()) {
-
             BigDecimal amountToTransfer = transfer.getAmount();
             Account accountFrom = accountDao.getAccountByAccountId(transfer.getAccountFrom());
             Account accountTo = accountDao.getAccountByAccountId(transfer.getAccountTo());
-
             accountFrom.sendMoney(amountToTransfer);
             accountTo.getMoney(amountToTransfer);
-
             transferDao.updateTransfer(transfer);
-
             accountDao.updateAccount(accountFrom);
             accountDao.updateAccount(accountTo);
         } else {
             transferDao.updateTransfer(transfer);
         }
-
     }
 }

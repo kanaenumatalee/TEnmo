b73fa013c30c1e61017fce497ea937499b5be77f
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:37:54 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferTypeDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferTypeDao.java
@@ -1,6 +1,8 @@
 package com.techelevator.tenmo.dao;
 
+
 import com.techelevator.tenmo.model.TransferType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -8,24 +10,20 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
+
 @Component
 public class JdbcTransferTypeDao implements TransferTypeDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
     public JdbcTransferTypeDao(DataSource dataSource){this.jdbcTemplate = new JdbcTemplate(dataSource);}
-
-
 
     @Override
     public TransferType getTransferTypeByDescription(String description) {
         String sql = "SELECT transfer_type_id, transfer_type_desc " +
                      "FROM transfer_type " +
                      "WHERE transfer_type_desc = ?";
-
         SqlRowSet result = jdbcTemplate.queryForRowSet(sql, description);
         TransferType transferType = null;
-
         if(result.next()) {
             int transferTypeId = result.getInt("transfer_type_id");
             String transferTypeDescription = result.getString("transfer_type_desc");
@@ -40,9 +38,7 @@ public class JdbcTransferTypeDao implements TransferTypeDao {
                      "FROM transfer_type " +
                      "WHERE transfer_type_id = ?";
         SqlRowSet result = jdbcTemplate.queryForRowSet(sql, transferTypeId);
-
         TransferType transferType = null;
-
         if(result.next()) {
             int transferTypeId2 = result.getInt("transfer_type_id");
             String transferTypeDesc = result.getString("transfer_type_desc");

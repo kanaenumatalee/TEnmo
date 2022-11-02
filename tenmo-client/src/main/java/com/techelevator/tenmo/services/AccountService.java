417bf1f21583af837529b095821d428ef828d51e
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 13:57:31 -0700

    Cleaned up codes

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -1,22 +1,24 @@
 package com.techelevator.tenmo.services;
 
+
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.util.BasicLogger;
+
+
 import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
-
 import java.math.BigDecimal;
 
+
 public class AccountService {
-
     private final String baseUrl;
-
     private RestTemplate restTemplate = new RestTemplate();
-
+    
+    
     public AccountService(String baseUrl) {
         this.baseUrl = baseUrl + "account/";
     }
@@ -24,7 +26,6 @@ public class AccountService {
     public BigDecimal getBalance(AuthenticatedUser authenticatedUser) {
         BigDecimal balance = null;
         HttpEntity entity = makeEntity(authenticatedUser);
-
         try {
             balance = restTemplate.exchange(baseUrl + "balance",
                                                 HttpMethod.GET,
@@ -40,11 +41,9 @@ public class AccountService {
         return balance;
     }
 
-
     public Account getAccountByUserId(AuthenticatedUser authenticatedUser, int userId) {
         Account account = null;
         HttpEntity entity = makeEntity(authenticatedUser);
-
         try {
             account = restTemplate.exchange(baseUrl + userId,
                                             HttpMethod.GET,
@@ -54,15 +53,12 @@ public class AccountService {
         } catch (ResourceAccessException e) {
             System.out.println("Failed to complete request due to server network issues. Please try again.");
         }
-
         return account;
     }
-
 
     public Account getAccountByAccountId(AuthenticatedUser authenticatedUser, int accountId) {
         Account account = null;
         HttpEntity entity = makeEntity(authenticatedUser);
-
         try {
             account = restTemplate.exchange(baseUrl + "users/" + accountId,
                                             HttpMethod.GET,
@@ -75,7 +71,6 @@ public class AccountService {
         }
         return account;
     }
-
 
     private HttpEntity makeEntity(AuthenticatedUser authenticatedUser) {
         HttpHeaders headers = new HttpHeaders();

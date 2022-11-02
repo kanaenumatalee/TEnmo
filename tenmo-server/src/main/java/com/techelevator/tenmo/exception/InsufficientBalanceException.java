a0585832b6ef80fea5fd75d55201a609b00dfc00
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:47:51 -0700

    Cleaned up codes

--- tenmo-server/src/main/java/com/techelevator/tenmo/exception/InsufficientBalanceException.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/exception/InsufficientBalanceException.java
@@ -1,5 +1,6 @@
 package com.techelevator.tenmo.exception;
 
+
 public class InsufficientBalanceException extends Exception{
     public InsufficientBalanceException() {
         super("Not enough balance in your account.");

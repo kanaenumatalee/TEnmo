b5d05d2d5e2795a335f57f86db77a1d10d81ff19
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:07:51 -0700

    Cleaned up codes

--- tenmo-client/src/main/java/com/techelevator/exception/NoUserFoundException.java
+++ tenmo-client/src/main/java/com/techelevator/exception/NoUserFoundException.java
@@ -1,5 +1,6 @@
 package com.techelevator.exception;
 
+
 public class NoUserFoundException extends Exception{
     public NoUserFoundException() {
         super("I'm sorry, no valid user was found.");

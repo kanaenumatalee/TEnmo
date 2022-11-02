38f54167cbb4dedbba62d2a1a9739d5c77bdf68d
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-11-02 14:08:12 -0700

    Cleaned up codes

--- tenmo-client/src/main/java/com/techelevator/exception/NotEnoughBalanceException.java
+++ tenmo-client/src/main/java/com/techelevator/exception/NotEnoughBalanceException.java
@@ -1,5 +1,6 @@
 package com.techelevator.exception;
 
+
 public class NotEnoughBalanceException extends Exception{
     public NotEnoughBalanceException() {
         super("You don't have enough balance for the transaction.");
